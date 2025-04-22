e8415f16cf4909fec57fe4b006998c3b7fd50bb2
Devansh Choudhary <devanshdc19@gmail.com>
2025-04-22 12:30:05 +0530

    Fix: Add todo-practice-project as normal folder

---
+++ todo-practice-project/todo-backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(package.json());
+
+app.post("/todo", (req,res)=>{
+    app.send()
+})
+
+
+app.get("/todos", (req,res)=>{
+
+})
+
+app.put("/completed", (req,res)=>{
+    
+})
