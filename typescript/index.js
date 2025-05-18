90988530ce78a54089d8374aa7405de113658432
Devansh Choudhary <devanshdc19@gmail.com>
2025-05-19 02:52:07 +0530

    basics of typescript

---
+++ typescript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const x = 1;
+console.log(x);
+function myname(name) {
+    console.log(name);
+    return 1;
+}
+myname("Devansh");
---
+++ typescript/index.ts
@@ -0,0 +1,9 @@
+const x: number = 1;
+console.log(x)
+
+function myname(name: string):number{
+    console.log(name)
+    return 1
+}
+
+myname("Devansh")
